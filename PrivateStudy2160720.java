415e0bdb595b573ecdf8419fed2128e7c6017014
tadaren <ptymaato@yahoo.co.jp>
2016-07-16 10:14:41 +0900

    Update PrivateStudy2160720.java

--- PrivateStudy2160720.java
+++ PrivateStudy2160720.java
@@ -12,7 +12,7 @@ public class PrivateStudy2160720 {
 		double b = stdIn.nextDouble();
 		System.out.println("a / b = " + Math.round(a / b));
 		System.out.println("a ^ b = " + Math.pow(a, b));
-		System.out.println("a と b の小さい方は .min(a, b));
+		System.out.println("a と b の小さい方は " + Math.min(a, b));
 		System.out.println("ルートa + ルートb = " + (Math.sqrt(a) + Math.sqrt(b)));
 		System.out.println("乱数a = "+Math.random()+", 乱数b = " +Math.random());
 	}
