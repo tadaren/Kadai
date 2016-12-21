2a54ab760ea2c6623e45a66236b5b71a58aead85
Tadaren <ptymaato@yahoo.co.jp>
2016-12-21 10:49:04 +0900

    Add files via upload

--- 161221/Practice16122101.java
+++ 161221/Practice16122101.java
@@ -1,6 +1,23 @@
-public class Practice161221{
-	public static void main(String[] args){
-		Scanner stdIn = new Scanner(System.in);
-		
-	}
+import java.util.Scanner;
+
+public class Practice16122101{
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        int[] input = new int[stdIn.nextInt()];
+        for(int i = 0; i < input.length; i++){
+            input[i] = stdIn.nextInt();
+        }
+        int max = input[0];
+        int min = input[0];
+        for(int element : input){
+            if(max < element){
+                max = element;
+            }
+            if(min > element){
+                min = element;
+            }
+        }
+        System.out.println("max : " + max);
+        System.out.println("min : " + min);
+    }
 }
